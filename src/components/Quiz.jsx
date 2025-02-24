a1d7c12a347af81db6b84e2b8a83cab641167a13
kachakaran6 <kachakaran06@gmail.com>
2025-02-24 21:21:23 +0530

    fifth

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./ans.css";
 import { data } from "../assets/data";
 
@@ -6,6 +6,13 @@ const Quiz = () => {
   let [index, setIndex] = useState(1);
   let [question, setQuestion] = useState(data[index]);
   let [lock, setLock] = useState(false);
+  let [score, setScore] = useState(0);
+  let [result, setResult] = useState(false);
+
+  let opt1 = useRef(null);
+  let opt2 = useRef(null);
+  let opt3 = useRef(null);
+  let opt4 = useRef(null);
 
   let option_array = [opt1, opt2, opt3, opt4];
 
@@ -14,11 +21,29 @@ const Quiz = () => {
       if (question.ans === ans) {
         e.target.classList.add("correct");
         setLock(true);
+        setScore((prev) => prev + 1);
       } else {
         e.target.classList.add("wrong");
         setLock(true);
         option_array[question.ans - 1].current.classList.add("correct");
       }
+    }
+  };
+
+  const next = () => {
+    if (lock === true) {
+      if (index === data.length - 1) {
+        setResult(true);
+        return 0;
+      }
+      setIndex(++index);
+      setQuestion(data[index]);
+      setLock(false);
+      option_array.map((option) => {
+        option.current.classList.remove("wrong");
+        option.current.classList.remove("correct");
+        return null;
+      });
     }
   };
   return (
@@ -30,51 +55,65 @@ const Quiz = () => {
         Quiz App
       </h1>
       <hr className="mb-6" style={{ borderColor: "#9FA2FC" }} />
+      {result ? (
+        <></>
+      ) : (
+        <>
+          <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+            {index + 1}. {question.question}
+          </h2>
 
-      <h2 className="text-2xl font-semibold text-gray-800 mb-4">
-        {index + 1}. {question.question}
-      </h2>
+          <ul className="list-disc pl-8 space-y-4">
+            <li
+              ref={opt1}
+              onClick={(e) => {
+                checkAns(e, 1);
+              }}
+              className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
+            >
+              {question.opt1}
+            </li>
+            <li
+              ref={opt2}
+              onClick={(e) => {
+                checkAns(e, 2);
+              }}
+              className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
+            >
+              {question.opt2}
+            </li>
+            <li
+              ref={opt3}
+              onClick={(e) => {
+                checkAns(e, 3);
+              }}
+              className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
+            >
+              {question.opt3}
+            </li>
+            <li
+              ref={opt4}
+              onClick={(e) => {
+                checkAns(e, 4);
+              }}
+              className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
+            >
+              {question.opt4}
+            </li>
+          </ul>
 
-      <ul className="list-disc pl-8 space-y-4">
-        <li
-          onClick={(e) => {
-            checkAns(e, 1);
-          }}
-          className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
-        >
-          {question.opt1}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, 2);
-          }}
-          className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
-        >
-          {question.opt2}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, 3);
-          }}
-          className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
-        >
-          {question.opt3}
-        </li>
-        <li
-          onClick={(e) => {
-            checkAns(e, 4);
-          }}
-          className="text-lg text-gray-700 hover:text-[#9FA2FC] cursor-pointer transition-all duration-300 ease-in-out"
-        >
-          {question.opt4}
-        </li>
-      </ul>
+          <button
+            onClick={next}
+            className="mt-6 px-6 py-2 bg-[#9FA2FC] text-white rounded-md hover:bg-[#8a90e6] transition-all duration-300 ease-in-out"
+          >
+            Next
+          </button>
 
-      <button className="mt-6 px-6 py-2 bg-[#9FA2FC] text-white rounded-md hover:bg-[#8a90e6] transition-all duration-300 ease-in-out">
-        Next
-      </button>
-
-      <div className="mt-4 text-center text-gray-600">1 of 5 Questions</div>
+          <div className="mt-4 text-center text-gray-600">
+            {index + 1}of {data.length} Questions
+          </div>
+        </>
+      )}
     </div>
   );
 };
